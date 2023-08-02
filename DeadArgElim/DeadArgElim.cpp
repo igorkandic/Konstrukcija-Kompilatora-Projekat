db88881420473cca06806edf0d636cf74e55f6ae
Igor <igorkandic2@gmail.com>
2023-08-02 19:25:57 +0200

    First commit

---
+++ DeadArgElim/DeadArgElim.cpp
@@ -0,0 +1,23 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+using namespace llvm;
+
+namespace {
+  struct DAEPass : public FunctionPass {
+    static char ID;
+    DAEPass() : FunctionPass(ID) {}
+
+    virtual bool runOnFunction(Function &F) {
+      errs() << "I saw a function called " << F.getName() << "!\n";
+      return false;
+    }
+  };
+}
+
+char DAEPass::ID = 0;
+
+static RegisterPass<DAEPass> X("deadargelim", "Dead Argument Elimination Pass",
+                                    false, false);
